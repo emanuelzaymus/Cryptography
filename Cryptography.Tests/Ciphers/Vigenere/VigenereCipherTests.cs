65651903c689413b4ea89b485d4caa125655de24
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2021-10-12 18:19:29 +0200

    Vigenere Cipher + tests

---
+++ Cryptography.Tests/Ciphers/Vigenere/VigenereCipherTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Cryptography.Alphabet;
+using Cryptography.Ciphers.Vigenere;
+using NUnit.Framework;
+
+namespace Cryptography.Tests.Ciphers.Vigenere
+{
+    [TestFixture]
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class VigenereCipherTests
+    {
+        [Test]
+        public void Encrypt_Alphabet_ShouldEncryptCorrectly()
+        {
+            VigenereCipher cipher = new(Alphabets.ALPHABET, "PASSWORD");
+
+            var encrypted = cipher.Encrypt(Alphabets.ALPHABET);
+
+            Assert.That(encrypted, Is.EqualTo("PBUVATXKXJCDIBFSFRKLQJNANZ"));
+        }
+
+        [Test]
+        public void Decrypt_EncryptedAlphabet_ShouldReturnPlainAlphabet()
+        {
+            VigenereCipher cipher = new(Alphabets.ALPHABET, "PASSWORD");
+
+            var decrypted = cipher.Decrypt("PBUVATXKXJCDIBFSFRKLQJNANZ");
+
+            Assert.That(decrypted, Is.EqualTo(Alphabets.ALPHABET));
+        }
+
+        [Test]
+        public void Encrypt_SecreteMessage_ShouldEncryptCorrectly()
+        {
+            VigenereCipher cipher = new(Alphabets.ALPHABET, "HESLO");
+
+            var encrypted = cipher.Encrypt("TOTOJETAJNASPRAVA");
+
+            Assert.That(encrypted, Is.EqualTo("ASLZXLXSUBHWHCOCE"));
+        }
+
+        [Test]
+        public void Decrypt_EncryptedMessage_ShouldReturnPlainMessage()
+        {
+            VigenereCipher cipher = new(Alphabets.ALPHABET, "HESLO");
+
+            var decrypted = cipher.Decrypt("ASLZXLXSUBHWHCOCE");
+
+            Assert.That(decrypted, Is.EqualTo("TOTOJETAJNASPRAVA"));
+        }
+    }
+}
--- Cryptography/Ciphers/Vigenere/VigenereCipher.cs
+++ Cryptography/Ciphers/Vigenere/VigenereCipher.cs
@@ -1,6 +1,6 @@
-﻿// ReSharper disable IdentifierTypo
-
-using System;
+﻿using System;
+using System.Linq;
+using Cryptography.Utilities;
 
 namespace Cryptography.Ciphers.Vigenere
 {
@@ -10,17 +10,50 @@ namespace Cryptography.Ciphers.Vigenere
 
         public VigenereCipher(string alphabet, string password) : base(alphabet)
         {
+            CheckPassword(password);
+
             _password = password;
+        }
+
+        private void CheckPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", nameof(password));
+            }
+
+            if (password.Any(ch => !Alphabet.Contains(ch)))
+            {
+                throw new ArgumentException("Password contains invalid character(s).", nameof(password));
+            }
         }
 
         protected override char CharEncryption(char ch, int stringCharIndex)
         {
-            throw new NotImplementedException();
+            return VigenereTransformation(ch, stringCharIndex, false);
         }
 
         protected override char CharDecryption(char ch, int stringCharIndex)
         {
-            throw new NotImplementedException();
+            return VigenereTransformation(ch, stringCharIndex, true);
+        }
+
+        private char VigenereTransformation(char ch, int stringCharIndex, bool isDecryption)
+        {
+            int alphabetCharIndex = GetAlphabetCharIndex(ch);
+
+            int passwordCharIndex = stringCharIndex % _password.Length;
+            char passwordChar = _password[passwordCharIndex];
+
+            int alphabetPasswordCharIndex = GetAlphabetCharIndex(passwordChar);
+
+            if (isDecryption)
+            {
+                alphabetPasswordCharIndex = -alphabetPasswordCharIndex;
+            }
+
+            int newCharIndex = Utils.PositiveModulo(alphabetCharIndex + alphabetPasswordCharIndex, Alphabet.Length);
+            return Alphabet[newCharIndex];
         }
     }
 }
